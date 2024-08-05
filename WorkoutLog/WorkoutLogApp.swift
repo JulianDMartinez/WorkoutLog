543ba0982efcef9c18812549c9f80f9ec1754321
Julian Martinez <julian.martinez.s@outlook.com>
2024-08-05 17:42:33 -0500

    Initial Commit

---
+++ WorkoutLog/WorkoutLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutLogApp.swift
+//  WorkoutLog
+//
+//  Created by Julian Martinez on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
